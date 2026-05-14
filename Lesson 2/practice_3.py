3cb05c69e12bcec04f01f818574f972d494bf6ff
hellyeahworld <sanjaryabulfazl@gmail.com>
2026-05-14 03:49:05 +0330

    Update practice_3.py
    تصحیح جزئی

--- Lesson 2/practice_3.py
+++ Lesson 2/practice_3.py
@@ -10,9 +10,9 @@ elif process == "/":
     print(result)
 
 elif process == "+":
-    result = number_1 - number_2
+    result = number_1 + number_2
     print(result)
 
 elif process == "-":
-    result = number_1 + number_2
+    result = number_1 - number_2
     print(result)
