1c857a89f957554efa0710c80f0bf91fa8facc8e
hellyeahworld <sanjaryabulfazl@gmail.com>
2026-05-14 03:51:03 +0330

    Update practice_2.py
    تغییرات جزئی

--- Lesson 2/practice_2.py
+++ Lesson 2/practice_2.py
@@ -6,6 +6,7 @@ user_choice = int(input("please enter your choice(1, 2, 3)"))
 
 if user_choice == computer_choice:
     print("Mosavi! ")
-elif (user_choice == 1 and computer_choice == 3) or (user_choice == 2 and computer_choice == 1) or (user_choice == 3 and computer_choice == 2):    print("You won! ")
+elif (user_choice == 1 and computer_choice == 3) or (user_choice == 2 and computer_choice == 1) or (user_choice == 3 and computer_choice == 2):
+    print("You won! ")
 else:
     print("you lost! ")
